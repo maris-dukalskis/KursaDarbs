670b513b689f3b698171270ae15a2efa033a5104
DaLabi <148067931+maris-dukalskis@users.noreply.github.com>
2023-11-06 13:10:32 +0200

    player class getters and setters

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,10 +1,61 @@
 package model;
 
 public class Player {
-	
+
 	private String name;
 	private String password;
-	private int moveCount;
+	private int moveCount = 0;
 	private GameState gameState;
+
+	public Player(String name, String password, GameState gameState) {
+		setName(name);
+		setPassword(password);
+		setGameState(gameState);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		if (name != null && name.matches("[A-Z]{1}[a-z]+")) {
+			this.name = name;
+		} else {
+			this.name = "Invalid name";
+		}
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) { // move check to login page
+		if (password != null && password.matches("^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$")) { // 8 characters,atleast 1
+																							// letter, one number
+			this.password = password;
+		} else {
+			this.password = null;
+		}
+	}
+
+	public int getMoveCount() {
+		return moveCount;
+	}
+
+	public void increaseMoveCount() {
+		this.moveCount++;
+	}
+
+	public GameState getGameState() {
+		return gameState;
+	}
+
+	public void setGameState(GameState gameState) {
+		if (gameState != null) {
+			this.gameState = gameState;
+		} else {
+			this.gameState = GameState.NORMAL;
+		}
+	}
 
 }
--- src/main/java/service/MainService.java
+++ src/main/java/service/MainService.java
@@ -2,8 +2,11 @@ package service;
 
 public class MainService {
 
-	// TODO
-	//	coinflip to choose colors
+//	 	TODO
+//		coinflip to choose colors
+//	 	generate getters and setters
+//	 	create algorithms for each piece
+//		
 
 	public static void main(String[] args) {
 
