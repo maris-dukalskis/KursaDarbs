fa1148b8c96267ef9236ae946bb2dfbc04e12a9b
sookrat666 <oskars.kracis@gmail.com>
2023-11-08 13:57:49 +0200

    Papildināju Piece klasi

--- src/main/java/model/Piece.java
+++ src/main/java/model/Piece.java
@@ -1,10 +1,61 @@
 package model;
 
 public class Piece {
-
+	//variables
 	private Color color;
 	private PieceType type;
 	private Tile tile;
 	private boolean isInPlay;
-
+	
+	//Konstruktors
+	public Piece(Color inputColor, PieceType inputType, Tile inputTile, boolean inputIsInPlay) {
+	    setColor(inputColor);
+	    setType(inputType);
+	    setTile(tile);
+	    setIsInPlay(isInPlay);
+	}	
+	//getteri	
+	public Color getColor() {
+		return color;
+	}
+	public PieceType getPieceType() {
+		return type;
+	}	
+	public Tile getTile() {
+		return tile;
+	}	
+	public boolean getIsInPlay() {
+		return isInPlay;
+	}	
+	//setteri	
+	public void setColor(Color inputColor) {
+		if(inputColor!=null && inputColor instanceof Color) {
+			color=inputColor;
+		}
+		else {
+			color=Color.BLACK;
+		}
+	}
+	public void setType(PieceType inputType) {
+		if(inputType!=null && inputType instanceof PieceType) {
+			type=inputType;
+		}
+		else {
+			type=PieceType.PAWN;
+		}
+	}
+	public void setTile(Tile inputTile) {
+		if (inputTile != null) {
+			tile = inputTile;
+		} else {
+			tile = null;
+		}
+	}
+	public void setIsInPlay(boolean inputIsInPlay) {
+		if(inputIsInPlay==true || inputIsInPlay==false) {
+		isInPlay = inputIsInPlay;
+	}	else {
+		isInPlay=true;
+		}
+	}
 }
