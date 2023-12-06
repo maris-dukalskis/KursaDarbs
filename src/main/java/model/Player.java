7faf53563f5bf38e455a3b284e22687140b2ca9d
Martins Z <112480628+Zuivids@users.noreply.github.com>
2023-12-06 18:39:35 +0200

    Pievienoju mainīgos no SceneBuilder

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,11 +1,21 @@
 package model;
 
-public class Player {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 
+public class Player {
+	@FXML
 	private String name;
 	private int moveCount = 0;
 	private GameState gameState;
 	private Color color;
+	private TextField player1_input_text;
+	private TextField player2_input_text;
+	private ToggleButton player1_yes_option , player1_no_option , player2_yes_option , player2_no_option;
+	private Button game_start_button;
+
 
 	public Player(String name, GameState gameState, Color color) {
 		setName(name);
