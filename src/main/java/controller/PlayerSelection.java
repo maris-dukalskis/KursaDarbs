817e298765ec705d65e1a4b7500ddf8a09b9ff30
Martins Z <112480628+Zuivids@users.noreply.github.com>
2023-12-06 19:00:14 +0200

    Pievienoju PlayerSelection klasi

---
+++ src/main/java/controller/PlayerSelection.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
+public class PlayerSelection {
+    @FXML
+    private TextField player1_input_text;
+    private TextField player2_input_text;
+    private ToggleButton player1_yes_option , player1_no_option , player2_yes_option , player2_no_option;
+    private Button game_start_button;
+
+
+}
