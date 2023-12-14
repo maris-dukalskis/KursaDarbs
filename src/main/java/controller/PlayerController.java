5037eed4cd67553d3914b55d1cf58954f3dab7c4
DaLabi <maris.dukalskis@gmail.com>
2023-12-14 15:22:59 +0200

    lil fix

--- src/main/java/controller/PlayerController.java
+++ src/main/java/controller/PlayerController.java
@@ -13,33 +13,33 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class PlayerController {
-    @FXML
-    private TextField player1_input_text;
-    @FXML
-    private TextField player2_input_text;
-    @FXML
-    private ToggleButton player1_yes_option , player1_no_option , player2_yes_option , player2_no_option;
-    @FXML
-    private Button game_start_button;
-    private String text1, text2;
-    private Stage primaryStage;
+	@FXML
+	private TextField player1_input_text;
+	@FXML
+	private TextField player2_input_text;
+	@FXML
+	private ToggleButton player1_yes_option, player1_no_option, player2_yes_option, player2_no_option;
+	@FXML
+	private Button game_start_button;
+	private String text1, text2;
+	private Stage primaryStage;
 
-    @FXML
-    private void startGame(ActionEvent event)throws IOException {
-        text1 = player1_input_text.getText();
-        text2 = player2_input_text.getText();
-        if(text1 != null){
-            text1 = "Player1";
-        }
-        if(text2 != null){
-            text2 = "Player2";
-        }
-        Scene myScene = FXMLLoader.load(getClass().getResource("/GameScene.fxml"));
-        primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        primaryStage.setScene(myScene);
-        primaryStage.show();
-        primaryStage.setResizable(false);
-        primaryStage.setTitle(text1+" vs "+text2);
-    }
+	@FXML
+	private void startGame(ActionEvent event) throws IOException {
+		text1 = player1_input_text.getText();
+		text2 = player2_input_text.getText();
+		if (text1.equals("")) {
+			text1 = "Player1";
+		}
+		if (text2.equals("")) {
+			text2 = "Player2";
+		}
+		Scene myScene = FXMLLoader.load(getClass().getResource("/GameScene.fxml"));
+		primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		primaryStage.setScene(myScene);
+		primaryStage.show();
+		primaryStage.setResizable(false);
+		primaryStage.setTitle(text1 + " vs " + text2);
+	}
 
 }
