bc5fc68e3e8dab9d0c31887aa7172537b9cc0bd7
SibillaZ <sibillazubricka@gmail.com>
2024-01-17 17:15:54 +0200

    TutorialSceneController

---
+++ src/main/java/controller/TutorialSceneController.java
@@ -0,0 +1,148 @@
+package controller;
+
+
+	import javafx.fxml.FXML;
+	import javafx.scene.control.Button;
+	import javafx.scene.control.Label;
+	import javafx.scene.control.ScrollPane;
+	import javafx.scene.image.ImageView;
+	import javafx.scene.layout.AnchorPane;
+	import javafx.scene.layout.HBox;
+	import javafx.scene.layout.VBox;
+	import javafx.scene.text.Text;
+
+	public class TutorialSceneController {
+
+	    @FXML
+	    private AnchorPane AnchorPane;
+
+	    @FXML
+	    private Button ButtonGotIt;
+
+	    @FXML
+	    private Text FirstText;
+
+	    @FXML
+	    private HBox HBoxFirstText;
+
+	    @FXML
+	    private ImageView ImageBishop;
+
+	    @FXML
+	    private ImageView ImageKing;
+
+	    @FXML
+	    private ImageView ImageKnight;
+
+	    @FXML
+	    private ImageView ImagePawn;
+
+	    @FXML
+	    private ImageView ImageQueen;
+
+	    @FXML
+	    private ImageView ImageRook;
+
+	    @FXML
+	    private Label LabelBishop;
+
+	    @FXML
+	    private Label LabelCastling;
+
+	    @FXML
+	    private Label LabelCheckmate;
+
+	    @FXML
+	    private Label LabelChessTutorial;
+
+	    @FXML
+	    private Label LabelCkeck;
+
+	    @FXML
+	    private Label LabelDraw;
+
+	    @FXML
+	    private Label LabelKing;
+
+	    @FXML
+	    private Label LabelKnight;
+
+	    @FXML
+	    private Label LabelPawn;
+
+	    @FXML
+	    private Label LabelPawnPromotion;
+
+	    @FXML
+	    private Label LabelPieces;
+
+	    @FXML
+	    private Label LabelQueen;
+
+	    @FXML
+	    private Label LabelRook;
+
+	    @FXML
+	    private Label LabelSpecialMoves;
+
+	    @FXML
+	    private Label LabelSpecialStates;
+
+	    @FXML
+	    private Label LabelWhoMakesTheFirstMove;
+
+	    @FXML
+	    private ScrollPane ScrollPlane;
+
+	    @FXML
+	    private Text TextBishop;
+
+	    @FXML
+	    private Text TextCastling;
+
+	    @FXML
+	    private Text TextCheck;
+
+	    @FXML
+	    private Text TextCheckmate;
+
+	    @FXML
+	    private Text TextDraw;
+
+	    @FXML
+	    private Text TextFirstMove;
+
+	    @FXML
+	    private Text TextKing;
+
+	    @FXML
+	    private Text TextKnight;
+
+	    @FXML
+	    private Text TextPawn;
+
+	    @FXML
+	    private Text TextPawnPromotion;
+
+	    @FXML
+	    private Text TextPieces;
+
+	    @FXML
+	    private Text TextQueen;
+
+	    @FXML
+	    private Text TextRook;
+
+	    @FXML
+	    private Text TextSpecialMoves;
+
+	    @FXML
+	    private VBox VBoxPawn;
+
+	    @FXML
+	    private VBox VBoxTut;
+
+
+}
+
+
