1352b6c499626731830dc688e5c2d0a0a4fbc812
SibillaZ <sibillazubricka@gmail.com>
2024-01-17 17:33:37 +0200

    Button "Got it" darbība

--- src/main/java/controller/ResultController.java
+++ src/main/java/controller/ResultController.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.event.ActionEvent;
 
 
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
--- src/main/java/controller/TutorialSceneController.java
+++ src/main/java/controller/TutorialSceneController.java
@@ -1,17 +1,26 @@
 package controller;
 
 
-	import javafx.fxml.FXML;
-	import javafx.scene.control.Button;
-	import javafx.scene.control.Label;
-	import javafx.scene.control.ScrollPane;
-	import javafx.scene.image.ImageView;
-	import javafx.scene.layout.AnchorPane;
-	import javafx.scene.layout.HBox;
-	import javafx.scene.layout.VBox;
-	import javafx.scene.text.Text;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 	public class TutorialSceneController {
+		
+		private Stage primaryStage;
 
 	    @FXML
 	    private AnchorPane AnchorPane;
@@ -141,6 +150,21 @@ package controller;
 
 	    @FXML
 	    private VBox VBoxTut;
+	    
+	    
+	    @FXML
+	    public void GotItClick(ActionEvent event) throws IOException {
+	 	   Object source = event.getSource();
+	 	   if (source instanceof Button && ((Button) source).getId().equals("ButtonGotIt")) {
+	 		   
+	 		 Scene myScene = FXMLLoader.load(getClass().getResource("/PlayerSelection.fxml"));
+	     	primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	     	primaryStage.setScene(myScene);
+	     	primaryStage.show();
+	     	primaryStage.setResizable(false);
+	     	primaryStage.setTitle("Player selection");  
+	 	   }	
+	     }
 
 
 }
