302f4e3c9e3b9b11b762557301c7f290a61220e4
Martins Z <112480628+Zuivids@users.noreply.github.com>
2024-01-18 15:47:28 +0200

    Pārtaisīju tutorialScene

--- src/main/java/controller/TutorialSceneController.java
+++ src/main/java/controller/TutorialSceneController.java
@@ -26,136 +26,13 @@ import javafx.stage.Stage;
 	    private AnchorPane AnchorPane;
 
 	    @FXML
-	    private Button ButtonGotIt;
+	    private Button gotItButton;
 
-	    @FXML
-	    private Text FirstText;
-
-	    @FXML
-	    private HBox HBoxFirstText;
-
-	    @FXML
-	    private ImageView ImageBishop;
-
-	    @FXML
-	    private ImageView ImageKing;
-
-	    @FXML
-	    private ImageView ImageKnight;
-
-	    @FXML
-	    private ImageView ImagePawn;
-
-	    @FXML
-	    private ImageView ImageQueen;
-
-	    @FXML
-	    private ImageView ImageRook;
-
-	    @FXML
-	    private Label LabelBishop;
-
-	    @FXML
-	    private Label LabelCastling;
-
-	    @FXML
-	    private Label LabelCheckmate;
-
-	    @FXML
-	    private Label LabelChessTutorial;
-
-	    @FXML
-	    private Label LabelCkeck;
-
-	    @FXML
-	    private Label LabelDraw;
-
-	    @FXML
-	    private Label LabelKing;
-
-	    @FXML
-	    private Label LabelKnight;
-
-	    @FXML
-	    private Label LabelPawn;
-
-	    @FXML
-	    private Label LabelPawnPromotion;
-
-	    @FXML
-	    private Label LabelPieces;
-
-	    @FXML
-	    private Label LabelQueen;
-
-	    @FXML
-	    private Label LabelRook;
-
-	    @FXML
-	    private Label LabelSpecialMoves;
-
-	    @FXML
-	    private Label LabelSpecialStates;
-
-	    @FXML
-	    private Label LabelWhoMakesTheFirstMove;
-
-	    @FXML
-	    private ScrollPane ScrollPlane;
-
-	    @FXML
-	    private Text TextBishop;
-
-	    @FXML
-	    private Text TextCastling;
-
-	    @FXML
-	    private Text TextCheck;
-
-	    @FXML
-	    private Text TextCheckmate;
-
-	    @FXML
-	    private Text TextDraw;
-
-	    @FXML
-	    private Text TextFirstMove;
-
-	    @FXML
-	    private Text TextKing;
-
-	    @FXML
-	    private Text TextKnight;
-
-	    @FXML
-	    private Text TextPawn;
-
-	    @FXML
-	    private Text TextPawnPromotion;
-
-	    @FXML
-	    private Text TextPieces;
-
-	    @FXML
-	    private Text TextQueen;
-
-	    @FXML
-	    private Text TextRook;
-
-	    @FXML
-	    private Text TextSpecialMoves;
-
-	    @FXML
-	    private VBox VBoxPawn;
-
-	    @FXML
-	    private VBox VBoxTut;
-	    
 	    
 	    @FXML
 	    public void GotItClick(ActionEvent event) throws IOException {
 	 	   Object source = event.getSource();
-	 	   if (source instanceof Button && ((Button) source).getId().equals("ButtonGotIt")) {
+	 	   if (source instanceof Button && ((Button) source).getId().equals("gotItButton")) {
 	 		   
 	 		 Scene myScene = FXMLLoader.load(getClass().getResource("/PlayerSelection.fxml"));
 	     	primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
